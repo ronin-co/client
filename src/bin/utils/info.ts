b3e9d977b3c5fcfc44e6c80067670d6a64ecb651
Leo Lamprecht <leo.lamprecht@icloud.com>
2024-05-31 19:15:43 +0200

    Ensure sleaker help message

--- src/bin/utils/info.ts
+++ src/bin/utils/info.ts
@@ -9,28 +9,25 @@ export const printVersion = () => {
 
 export const printHelp = () => {
   const text = chalkTemplate`
-    {bold.magenta ronin} — Automatically set the types for your RONIN project
+  {bold.magenta ronin} — Automatically set the types for your RONIN project
 
-    {bold USAGE}
+  {bold USAGE}
 
-        {bold $} {magenta ronin}
-        {bold $} {magenta ronin} login
-        {bold $} {magenta ronin} --help
-        {bold $} {magenta ronin} --version
+      {bold $} {magenta ronin}
+      {bold $} {magenta ronin} login
+      {bold $} {magenta ronin} --help
+      {bold $} {magenta ronin} --version
 
-    {bold COMMANDS}
+  {bold COMMANDS}
 
-        login                               Authenticate with RONIN (run by default for every command)
+      login                               Authenticate with RONIN (run by default for every command)
+      init [space]                        Initialize the TypeScript types for a given space
 
-        init [space]                        Initialize the TypeScript types for a given space
+  {bold OPTIONS}
 
-    {bold OPTIONS}
-
-        -h, --help                          Shows this help message
-
-        -v, --version                       Displays the current version of serve
-
-        -d, --debug                         Show debugging information
+      -h, --help                          Shows this help message
+      -v, --version                       Displays the current version of the CLI
+      -d, --debug                         Show debugging information
   `;
   console.log(text);
   process.exit(0);
