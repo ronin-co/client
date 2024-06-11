eb941983036f29f7917e7611109e3ee0f9d60df4
Leo Lamprecht <leo.lamprecht@icloud.com>
2024-06-11 16:19:08 +0200

    Added new `/utils` export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { runQueriesWithStorageAndHooks as runQueries } from './queries';
-import { processStorableObjects } from './storage';
 import { createSyntaxFactory } from './syntax';
 
 const { create, get, set, drop, count, batch } = createSyntaxFactory({});
 
-export { create, get, set, drop, count, batch, runQueries, processStorableObjects };
+export { create, get, set, drop, count, batch };
 export type { RONIN } from './types/codegen';
 export default createSyntaxFactory;
---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { runQueriesWithStorageAndHooks as runQueries } from '../queries';
+import { processStorableObjects } from '../storage';
+import { getSyntaxProxy } from '../syntax/utils';
+
+export { runQueries, processStorableObjects, getSyntaxProxy };
