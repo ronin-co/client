bfbcb9b40f3e4c838464e3910445507c439ba2a0
Dilshod Turobov <deebov@yandex.com>
2024-07-04 21:03:50 +0200

    Add utils for config

---
+++ src/bin/utils/config.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+
+interface Config {
+  spaceId?: string;
+  schemasDir?: string;
+}
+
+export const saveConfig = (config: Config) => {
+  const configDir = path.join(process.cwd(), '.ronin');
+
+  if (!fs.existsSync(configDir)) {
+    fs.mkdirSync(configDir);
+  }
+
+  const configPath = path.join(configDir, 'config.json');
+  let existingConfig: Config = {};
+
+  if (fs.existsSync(configPath)) {
+    existingConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  }
+
+  fs.writeFileSync(configPath, JSON.stringify({ ...existingConfig, ...config }, null, 2));
+};
+
+export const readConfig = (): Config => {
+  const configPath = path.join(process.cwd(), '.ronin', 'config.json');
+
+  if (!fs.existsSync(configPath)) {
+    return {};
+  }
+
+  return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+};
