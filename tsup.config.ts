7bfd8b51704cf7070c036e055a862b82852cecb4
Dilshod Turobov <deebov@yandex.com>
2024-07-04 21:01:32 +0200

    Add esbuild plugin

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,40 @@
+import type { Plugin } from 'esbuild';
+import * as fs from 'fs';
 import { defineConfig } from 'tsup';
+
+const injectShimsPlugin: Plugin = {
+  name: 'inject-shims-plugin',
+  setup(build) {
+    const outputFilePath = 'dist/bin/index.js';
+    const scriptFilePath = './scripts/cjs-shim.ts';
+    const startFromLine = 1;
+
+    build.onEnd((result) => {
+      // Read the script content from the specified file
+      let scriptToInject = '';
+      try {
+        scriptToInject = fs.readFileSync(scriptFilePath, 'utf8');
+      } catch (err) {
+        console.error(`Failed to read the script file ${scriptFilePath}:`, err);
+        return;
+      }
+
+      result.outputFiles = result.outputFiles!.map((outputFile) => {
+        if (!outputFile.path.endsWith(outputFilePath)) {
+          return outputFile;
+        }
+
+        const text = outputFile.text.split('\n');
+        text.splice(startFromLine, 0, scriptToInject);
+
+        return {
+          ...outputFile,
+          text: text.join('\n'),
+        };
+      });
+    });
+  },
+};
 
 export default defineConfig({
   entry: [
@@ -10,4 +46,5 @@ export default defineConfig({
   ],
   format: 'esm',
   dts: true,
+  esbuildPlugins: [injectShimsPlugin],
 });
