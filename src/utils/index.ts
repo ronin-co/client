2255ecb47abe8aa44abb2b8a95df8ea9dc62b222
Leo Lamprecht <leo@ronin.co>
2024-07-10 19:59:22 +0200

    Export `InvalidQueryError` class (#93)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { runQueriesWithStorageAndHooks as runQueries } from '@/src/queries';
 import { processStorableObjects } from '@/src/storage';
 import { getBatchProxy, getSyntaxProxy } from '@/src/syntax/utils';
+import { InvalidQueryError } from '@/src/utils/errors';
 
-export { runQueries, processStorableObjects, getSyntaxProxy, getBatchProxy };
+export { runQueries, processStorableObjects, getSyntaxProxy, getBatchProxy, InvalidQueryError };
