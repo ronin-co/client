564c0ad25f65c1e428a35af2426272160173c902
Dilshod Turobov <deebov@yandex.com>
2024-08-21 22:07:11 +0200

    Improve schema types

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,113 +1,122 @@
+export type BasicSchemaField = {
+  id: string;
+  slug: string;
+  name: string;
+  description?: string | undefined;
+  type: 'group' | 'date' | 'blob' | 'boolean' | 'number';
+  required?: boolean | undefined;
+  unique?: boolean | undefined;
+};
+
+export type StringSchemaField = {
+  id: string;
+  slug: string;
+  name: string;
+  description?: string | undefined;
+  required?: boolean | undefined;
+  unique?: boolean | undefined;
+  type: 'string';
+  displayAs?: 'single-line' | 'multi-line' | 'secret';
+};
+
+export type ListSchemaField = {
+  id: string;
+  slug: string;
+  name: string;
+  description?: string | undefined;
+  required?: boolean | undefined;
+  unique?: boolean | undefined;
+  type: 'list';
+  shape: (
+    | {
+        id: string;
+        slug: string;
+        name: string;
+        description?: string | undefined;
+        type: 'group' | 'date' | 'blob' | 'boolean' | 'number';
+        required?: boolean | undefined;
+        unique?: boolean | undefined;
+      }
+    | {
+        id: string;
+        slug: string;
+        name: string;
+        description?: string | undefined;
+        required?: boolean | undefined;
+        unique?: boolean | undefined;
+        type: 'string';
+        displayAs?: 'single-line' | 'multi-line' | 'secret';
+      }
+    | {
+        id: string;
+        slug: string;
+        name: string;
+        description?: string | undefined;
+        required?: boolean | undefined;
+        unique?: boolean | undefined;
+        type: 'reference';
+        schema: string | null;
+        space?: string | undefined;
+        action?:
+          | {
+              onDelete: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
+              onUpdate: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
+            }
+          | undefined;
+      }
+    | {
+        id: string;
+        slug: string;
+        name: string;
+        description?: string | undefined;
+        required?: boolean | undefined;
+        unique?: boolean | undefined;
+        type: 'json';
+        displayAs?: 'rich-text' | undefined;
+      }
+  )[];
+};
+
+export type ReferenceSchemaField = {
+  id: string;
+  slug: string;
+  name: string;
+  description?: string | undefined;
+  required?: boolean | undefined;
+  unique?: boolean | undefined;
+  type: 'reference';
+  schema: string | null;
+  space?: string | undefined;
+  action?:
+    | {
+        onDelete: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
+        onUpdate: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
+      }
+    | undefined;
+};
+
+export type JSONSchemaField = {
+  id: string;
+  slug: string;
+  name: string;
+  description?: string | undefined;
+  required?: boolean | undefined;
+  unique?: boolean | undefined;
+  type: 'json';
+  displayAs?: 'rich-text' | undefined;
+};
+
+export type SchemaField =
+  | BasicSchemaField
+  | StringSchemaField
+  | ListSchemaField
+  | ReferenceSchemaField
+  | JSONSchemaField;
+
 export type Schema = {
   id: string;
   summary: string;
-  fields:
-    | (
-        | {
-            id: string;
-            slug: string;
-            name: string;
-            description?: string | undefined;
-            type: 'group' | 'date' | 'blob' | 'boolean' | 'number';
-            required?: boolean | undefined;
-            unique?: boolean | undefined;
-          }
-        | {
-            id: string;
-            slug: string;
-            name: string;
-            description?: string | undefined;
-            required?: boolean | undefined;
-            unique?: boolean | undefined;
-            type: 'string';
-            displayAs?: 'single-line' | 'multi-line' | 'secret';
-          }
-        | {
-            id: string;
-            slug: string;
-            name: string;
-            description?: string | undefined;
-            required?: boolean | undefined;
-            unique?: boolean | undefined;
-            type: 'list';
-            shape: (
-              | {
-                  id: string;
-                  slug: string;
-                  name: string;
-                  description?: string | undefined;
-                  type: 'group' | 'date' | 'blob' | 'boolean' | 'number';
-                  required?: boolean | undefined;
-                  unique?: boolean | undefined;
-                }
-              | {
-                  id: string;
-                  slug: string;
-                  name: string;
-                  description?: string | undefined;
-                  required?: boolean | undefined;
-                  unique?: boolean | undefined;
-                  type: 'string';
-                  displayAs?: 'single-line' | 'multi-line' | 'secret';
-                }
-              | {
-                  id: string;
-                  slug: string;
-                  name: string;
-                  description?: string | undefined;
-                  required?: boolean | undefined;
-                  unique?: boolean | undefined;
-                  type: 'reference';
-                  schema: string | null;
-                  space?: string | undefined;
-                  action?:
-                    | {
-                        onDelete: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
-                        onUpdate: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
-                      }
-                    | undefined;
-                }
-              | {
-                  id: string;
-                  slug: string;
-                  name: string;
-                  description?: string | undefined;
-                  required?: boolean | undefined;
-                  unique?: boolean | undefined;
-                  type: 'json';
-                  displayAs?: 'rich-text' | undefined;
-                }
-            )[];
-          }
-        | {
-            id: string;
-            slug: string;
-            name: string;
-            description?: string | undefined;
-            required?: boolean | undefined;
-            unique?: boolean | undefined;
-            type: 'reference';
-            schema: string | null;
-            space?: string | undefined;
-            action?:
-              | {
-                  onDelete: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
-                  onUpdate: ('cascade' | 'clear' | 'restrict' | 'reset') | null;
-                }
-              | undefined;
-          }
-        | {
-            id: string;
-            slug: string;
-            name: string;
-            description?: string | undefined;
-            required?: boolean | undefined;
-            unique?: boolean | undefined;
-            type: 'json';
-            displayAs?: 'rich-text' | undefined;
-          }
-      )[]
-    | null;
+  fields: SchemaField[] | null;
   identifiers: {
     title: string | null;
     slug: string | null;
