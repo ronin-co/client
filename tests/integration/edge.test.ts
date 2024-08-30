b07095660abedd81b02995087e93d4d00378a2b7
Leo Lamprecht <leo@ronin.co>
2024-08-30 14:30:59 +0200

    Expose before/after values if data hooks are used (#125)
    * Use a single object for hook options
    
    * Renamed `autoSkipHooks` option to `autoSkip`
    
    * Revert "Renamed `autoSkipHooks` option to `autoSkip`"
    
    This reverts commit 72009b6bc5b4e3c449e7c6e38e49d50576f52997.
    
    * Revert "Use a single object for hook options"
    
    This reverts commit 14b2b12e63714d7499b2b5716f5baac6078a435f.
    
    * Ensure cleaner logic
    
    * Make it work
    
    * Made `before` hooks work again
    
    * Ensure more working tests
    
    * Ensure more working tests
    
    * Ensure green tests
    
    * Increase test coverage
    
    * Added test for after hooks
    
    * Added another test
    
    * Added test for "drop" queries
    
    * Ensure full test coverage
    
    * Ensure helpful comments
    
    * Added simpler function

--- tests/integration/edge.test.ts
+++ tests/integration/edge.test.ts
@@ -1,23 +1,11 @@
 import { AsyncLocalStorage } from 'node:async_hooks';
 
-import { beforeEach, describe, expect, mock, spyOn, test } from 'bun:test';
+import { describe, expect, spyOn, test } from 'bun:test';
 
 import createSyntaxFactory from '@/src/index';
 import { runQueriesWithHooks } from '@/src/utils/data-hooks';
 
-const mockFetch = mock(async () => {
-  return Response.json({
-    results: [],
-  });
-});
-
-global.fetch = mockFetch;
-
 describe('edge runtime', () => {
-  beforeEach(() => {
-    mockFetch.mockClear();
-  });
-
   test('invoke `ronin` from an edge runtime without passing a token', async () => {
     let error: Error | undefined;
 
@@ -88,6 +76,20 @@ describe('edge runtime', () => {
     global.process = undefined as unknown as NodeJS.Process;
 
     await runQueriesWithHooks(queries, {
+      fetch: async () => {
+        return Response.json({
+          results: [
+            {
+              record: {
+                id: '1',
+                handle: 'leo',
+                firstName: 'Leo',
+                lastName: 'Lamprecht',
+              },
+            },
+          ],
+        });
+      },
       hooks: {
         account: {
           afterCreate: async function () {
@@ -109,9 +111,112 @@ describe('edge runtime', () => {
 
     expect(promisesToAwait.length).toBeGreaterThan(0);
 
-    // Wait for asynchronous actions to finish.
+    // Wait for asynchronous "after" data hooks to finish.
     expect(hookInvocationHappened).not.toHaveBeenCalled();
     await Promise.all(promisesToAwait);
     expect(hookInvocationHappened).toHaveBeenCalled();
+  });
+
+  test('invoke `ronin` from an edge runtime with `waitUntil` set and error being thrown', async () => {
+    const queries = [
+      {
+        create: { account: { with: { handle: 'leo' } } },
+      },
+    ];
+
+    const errorText = 'I am an error';
+    const promisesToAwait: Promise<unknown>[] = [];
+
+    // Simulate a web runtime.
+    const oldProcess = global.process;
+    global.process = undefined as unknown as NodeJS.Process;
+
+    await runQueriesWithHooks(queries, {
+      fetch: async () => {
+        return Response.json({
+          results: [
+            {
+              record: {
+                id: '1',
+                handle: 'leo',
+                firstName: 'Leo',
+                lastName: 'Lamprecht',
+              },
+            },
+          ],
+        });
+      },
+      hooks: {
+        account: {
+          afterCreate: async function () {
+            throw new Error(errorText);
+          },
+        },
+      },
+      waitUntil: (promise) => {
+        promisesToAwait.push(promise);
+      },
+      asyncContext: new AsyncLocalStorage(),
+    });
+
+    // Restore the old runtime.
+    global.process = oldProcess;
+
+    // Wait for asynchronous "after" data hooks to finish.
+    expect(promisesToAwait[0]).rejects.toThrow(errorText);
+  });
+
+  test('invoke `ronin` from an edge runtime with `waitUntil` set and ensure hidden result', async () => {
+    const queries = [
+      {
+        create: { account: { with: { handle: 'leo' } } },
+      },
+    ];
+
+    const promisesToAwait: Promise<unknown>[] = [];
+
+    // Simulate a web runtime.
+    const oldProcess = global.process;
+    global.process = undefined as unknown as NodeJS.Process;
+
+    await runQueriesWithHooks(queries, {
+      fetch: async () => {
+        return Response.json({
+          results: [
+            {
+              record: {
+                id: '1',
+                handle: 'leo',
+                firstName: 'Leo',
+                lastName: 'Lamprecht',
+              },
+            },
+          ],
+        });
+      },
+      hooks: {
+        account: {
+          afterCreate: async function () {
+            // Sleep for 50 milliseconds to simulate an asynchronous action.
+            await new Promise((resolve) => setTimeout(resolve, 50));
+          },
+        },
+      },
+      waitUntil: (promise) => {
+        promisesToAwait.push(promise);
+      },
+      asyncContext: new AsyncLocalStorage(),
+    });
+
+    // Restore the old runtime.
+    global.process = oldProcess;
+
+    // Wait for asynchronous "after" data hooks to finish.
+    const result = await Promise.all(promisesToAwait);
+
+    // Ensure that the internal results of the hook run are not being exposed.
+    // In other words, the promises handed to `waitUntil` must not resolve with
+    // any value, since their purpose is to run asynchronous "after" hooks.
+    expect(result).toMatchObject([undefined]);
   });
 });
