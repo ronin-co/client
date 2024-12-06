8b633f088efc7acb3f450f9b878419eeecd44be3
Leo Lamprecht <leo@ronin.co>
2024-12-06 12:37:56 +0100

    Import query symbols from compiler

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,16 +1,2 @@
-/**
- * A list of placeholders that can be located inside queries after those queries were
- * serialized into JSON objects.
- *
- * These placeholders are used to represent special keys and values. For example, if a
- * query is nested into a query, the nested query will be marked with `__RONIN_QUERY`,
- * which allows for distinguishing that nested query from an object of instructions.
- */
-export const RONIN_SCHEMA_SYMBOLS = {
-  QUERY: '__RONIN_QUERY',
-  FIELD: '__RONIN_FIELD_',
-  VALUE: '__RONIN_VALUE',
-} as const;
-
 /** A list of all query types that update the database. */
 export const WRITE_QUERY_TYPES = ['add', 'set', 'remove', 'create', 'alter', 'drop'];
