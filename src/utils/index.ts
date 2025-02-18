8069492b00582dbd74decd55e4e918327c4a81a7
Leo Lamprecht <leo@ronin.co>
2025-02-18 09:54:21 +0100

    Export util for parsing blob objects (#68)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export { runQueriesWithStorageAndHooks as runQueries } from '@/src/queries';
-export { processStorableObjects } from '@/src/storage';
+export { processStorableObjects, isStorableObject } from '@/src/storage';
 export { InvalidResponseError } from '@/src/utils/errors';
