fef3b24581c5e9cd6ea68367906c2b4516b2a08f
Leo Lamprecht <leo@ronin.co>
2025-02-27 13:00:00 +0100

    Fixed query types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 import { DDL_QUERY_TYPES, DML_QUERY_TYPES_WRITE } from '@ronin/compiler';
 
 /** A list of all query types that update the database. */
-export const WRITE_QUERY_TYPES = [...DML_QUERY_TYPES_WRITE, DDL_QUERY_TYPES] as const;
+export const WRITE_QUERY_TYPES = [...DML_QUERY_TYPES_WRITE, ...DDL_QUERY_TYPES];
